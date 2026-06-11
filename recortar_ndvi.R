99f864e3f667b3794b11b976dc22fc87d81c44d9
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-11 16:41:47 -0300

    Importar e visualizar shapefile dos biomas

--- recortar_ndvi.R
+++ recortar_ndvi.R
@@ -7,3 +7,16 @@ library(tidyverse)
 library(terra)
 
 library(tidyterra)
+
+# Shapefile dos biomas ----
+
+## Importar ----
+
+biomas <- sf::st_read("lml_bioma_e250k_v20250911_A.shp")
+
+## Visualizar ----
+
+biomas
+
+ggplot() +
+  geom_sf(data = biomas, color = "black")

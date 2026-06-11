62c02f5cba7770800128e36e9b9e55317c5e286e
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-11 16:55:07 -0300

    Importar e visualizar o shapefile dos biomas

--- baixar_bioclim.R
+++ baixar_bioclim.R
@@ -10,3 +10,15 @@ library(terra)
 
 library(tidyverse)
 
+# Shapefile dos biomas ----
+
+## Importar ----
+
+biomas <- sf::st_read("lml_bioma_e250k_v20250911_A.shp")
+
+## Visualizar ----
+
+biomas
+
+ggplot() +
+  geom_sf(data = biomas, color = "black")

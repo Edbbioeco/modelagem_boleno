437708414c33e5cac30da202f55014a9f153d172
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-16 00:38:27 -0300

    Recortar os pontos para Fernando de Noronha

--- baixar_registros.R
+++ baixar_registros.R
@@ -34,5 +34,21 @@ fdn <- geobr::read_municipality(year = 2019) |>
 
 ## Visualizar ----
 
+fdn
+
 ggplot() +
   geom_sf(data = fdn, color = "black")
+
+# Recortar os registros para Fernando de Noronha ----
+
+registros_sf <- registros |>
+  dplyr::select(scientificName, decimalLongitude, decimalLatitude) |>
+  sf::st_as_sf(coords = c("decimalLongitude", "decimalLatitude"),
+               crs = fdn |> sf::st_crs()) |>
+  sf::st_intersection(fdn)
+
+registros_sf
+
+ggplot() +
+  geom_sf(data = fdn, color = "black") +
+  geom_sf(data = registros_sf, color = "black")

46eefb3bba7e9a0bc867826645937a14fdcffcbe
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-16 00:27:21 -0300

    Baixar dados GBIF

--- baixar_registros.R
+++ baixar_registros.R
@@ -5,3 +5,18 @@ library(rgbif)
 library(tidyverse)
 
 library(writexl)
+
+# Registros ----
+
+## Baixar ----
+
+registros <- rgbif::occ_data(scientificName = "Trachylepis atlantica",
+                             hasCoordinate = TRUE,
+                             limit = 1e4) %>%
+  .$data
+
+## Visualizar ----
+
+registros
+
+registros |> dplyr::glimpse()

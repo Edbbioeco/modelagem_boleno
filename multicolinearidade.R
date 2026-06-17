5953dbbea78eb001ce8af36716e4a26c6e99e58e
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-17 14:32:31 -0300

    Importar e visualizar as variáveis bioclimáticas

--- multicolinearidade.R
+++ multicolinearidade.R
@@ -27,3 +27,25 @@ registros <- readxl::read_xlsx("registros.xlsx")
 registros
 
 registros |> dplyr::glimpse()
+
+## Variáveis ambientais ----
+
+### Variáveis bioclimáticas ----
+
+#### Importar ----
+
+bio <- terra::rast("./rasters/bioclim/bioclim.tif")
+
+#### Visualizar ----
+
+bio
+
+purrr::map(1:19,
+           purrr::in_parallel(
+
+             ~ggplot() +
+               tidyterra::geom_spatraster(data = bio[[.x]]) +
+               scale_fill_viridis_c(na.value = "transparent")
+
+             ),
+           .progress = TRUE)

305a2e23f7acb7c668f8f8c0707d8b686f0682af
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-19 15:11:59 -0300

    Escolha das melhores repetições por algorítimo

--- modelagem.R
+++ modelagem.R
@@ -95,25 +95,21 @@ modelos
 
 modelos |> sdm::write.sdm("modelo.sdm")
 
-modelo <- sdm::read.sdm("modelo.sdm")
+modelos <- sdm::read.sdm("modelo.sdm")
 
 ## Selecionar os melhores modelos de cada algorírimo ----
 
-ind_modelos <- purrr::map_dbl(modelos,
-                              purrr::in_parallel(
+id_modelos <- modelo |>
+  sdm::getEvaluation() |>
+  dplyr::mutate(algoritimo = rep(c("gam",
+                                   "glm",
+                                   "maxent",
+                                   "maxlike"),
+                                 each = 10)) |>
+  dplyr::group_by(algoritimo) |>
+  dplyr::arrange(AUC |> dplyr::desc(),
+                 TSS |> dplyr::desc()) |>
+  dplyr::slice(1) |>
+  dplyr::pull(modelID)
 
-                                ~.x |>
-                                  sdm::getEvaluation() |>
-                                  dplyr::arrange(AUC |> dplyr::desc(),
-                                                 TSS |> dplyr::desc()) |>
-                                  dplyr::slice(1) |>
-                                  dplyr::pull(modelID)
-
-                                ),
-                              .progress = TRUE) |>
-  setNames(c("gam",
-             "glm",
-             "maxent",
-             "maxlike"))
-
-ind_modelos
+id_modelos
